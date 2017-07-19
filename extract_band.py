1c4f1d1818ce974d8135222bc31a49384b1a5696
Gene Christhopher Romuga <generomuga12693@gmail.com>
2017-07-19 10:43:01 +0800

    able to extract bands

--- extract_band.py
+++ extract_band.py
@@ -1,21 +1,43 @@
 from spectral import *
-import numpy as np
+import os
 
+#input directory
+modis_dir = os.getcwd()+'\\modis\\'
+#output directory
+modis_output = os.getcwd()+'\\output\\'
+#list of directory item
+modis_item = os.listdir(modis_dir)
 
-img = open_image('2003001_ref.hdr').read_band(0)
+def extract_band(dir,fname):
+	for i in xrange(0,7):
+		img = open_image(dir+fname).read_band(i)
+		file_save = open(modis_output+str(fname[:7])+str(i+1)+'.txt','w')
+		
+		#get the shape size
+		rows,cols = img.shape
+	
+		#saves the header
+		file_save.write('ncols\t'+str(cols)+'\n'+
+						'nrows\t'+str(rows)+'\n'+
+						'xllcorner\t12815693.052\n'+
+						'yllcorner\t1597502.247\n'+
+						'cellsize\t463.31272\n')
 
-file_save = open('band1.txt','w')
+		ctr = 0
+		for i in xrange(0,520):
+			for x in img[i]:
+				if ctr < 498:
+					ctr = ctr + 1
+					file_save.write('\t'+str('%.4f' % x)+'\t')
+				else:
+					file_save.write('\n')
+					ctr = 0
 
+def open_directory():
+	for filename in modis_item:
+		if filename.endswith(".hdr"):
+			print (modis_dir+filename)
+			extract_band(modis_dir,filename)
 
-ctr = 0
-
-for i in xrange(0,520):
-	for x in img[i]:
-		if ctr < 498:
-			ctr = ctr + 1
-			file_save.write(str(x)+'\t')
-		else:
-			file_save.write('\n')
-			ctr = 0
-
-
+if __name__ == '__main__':
+	open_directory()
